9f518d744e81e4bc292684bcc60116513c9bbee0
Divxsh <guptadivash70@gmail.com>
2023-08-29 22:16:17 +0530

    completed invitation update api

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -87,19 +87,19 @@ const getPlayers = async (req, res) => {
 
 		const userScheduledAccepted = {}
 		const userScheduledNotAccepted = {}
-		
+
 		usersInvites.forEach(invite => {
 			const match_d = invite.match_dt.toISOString().split("T")[0];
-			
+
 			if (invite.invitation_accepted)
 				userScheduledAccepted[match_d] = true;
-			else if (!userDetail._id.equals(invite.invitee_id)){
+			else if (!userDetail._id.equals(invite.invitee_id)) {
 				if (userScheduledNotAccepted[match_d])
 					userScheduledNotAccepted[match_d][invite.invitee_id] = true;
-				else 
-					userScheduledNotAccepted[match_d] = {[invite.invitee_id] : true}; 
+				else
+					userScheduledNotAccepted[match_d] = { [invite.invitee_id]: true };
 			}
-				
+
 		})
 
 		// Generate availability data
@@ -109,13 +109,13 @@ const getPlayers = async (req, res) => {
 			if (!userScheduledAccepted[date])
 				Object.entries(userDetailsMap).forEach(([key, userDetails], _) => {
 					let isAvailable = userInvitesMap[userDetails._id] && userInvitesMap[userDetails._id][date] ? false : true;
-					
+
 					if (isAvailable && (!userScheduledNotAccepted[date] || (userScheduledNotAccepted[date] && !userScheduledNotAccepted[date][userDetails._id]))) {
-							if (!userDetails.available_dates) {
-								userDetailsMap[key] = { ...userDetails, available_dates: [{ [date]: true }] }
-							}
-							else
-								userDetailsMap[key].available_dates.push({ [date]: true })
+						if (!userDetails.available_dates) {
+							userDetailsMap[key] = { ...userDetails, available_dates: [{ [date]: true }] }
+						}
+						else
+							userDetailsMap[key].available_dates.push({ [date]: true })
 					}
 				})
 		}
@@ -159,21 +159,45 @@ const sendInvitation = async (req, res) => {
 };
 
 const getInvitation = async (req, res) => {
-	const invitations = await Invitation.find({
-		inviter_id: req.user._id,
-	}).populate("invitee_id");
+	const invitations = await Invitation.find({inviter_id: req.user._id})
+										.populate("invitee_id");
 	res.status(200).json({ data: invitations });
 };
 
 const getInvites = async (req, res) => {
-	const invites = await Invitation.find({ invitee_id: req.user._id }).populate(
-		"inviter_id",
-	);
+	const invites = await Invitation.find({ invitee_id: req.user._id })
+									.populate("inviter_id",);
 	res.status(200).json({ data: invites });
 };
 
-const updateInviteStatus = (req, res) => {
-	// if one match request accepted for specific date the other request of taht specific date will be rejected 
+const updateInviteStatus = async (req, res) => {
+	const { invite_id, status } = req.body;
+	const { _id } = req.user;
+
+	if (!invite_id || !status)
+		res.status(404).send({ msg: "details are missing" })
+
+	try {
+		const updated = await Invitation.findByIdAndUpdate(
+			{ _id: invite_id }, 
+			{ 
+				$set: { 
+					invitation_status: status, 
+					invitation_accepted: status === "accepted" ? true : false 
+				} 
+			})
+
+		await Invitation.updateMany(
+			{ _id: { $ne: updated._id }, 
+			match_dt: updated.match_dt, 
+			$or: [{ inviter_id: _id }, { invitee_id: _id }] }, 
+			{ invitation_status: "rejected" }
+		)
+
+		res.status(200).send({ msg: "success" })
+	} catch (error) {
+		res.status(404).send({ msg: error.message })
+	}
 };
 
 module.exports = {
